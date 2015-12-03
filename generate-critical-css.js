46332b0cd7a0c28aefe4fb554dc6fecf16f0a1f2
Robin Drexler <drexler.robin@gmail.com>
2015-12-03 10:52:16 +0100

    Switch to phantomjs to reduce needed requests

--- generate-critical-css.js
+++ generate-critical-css.js
@@ -1,35 +1,47 @@
-const request = require('request');
-const cheerio = require('cheerio');
-const URL = require('url');
-const async = require('async');
 const mktemp = require('mktemp');
 const fs = require('fs');
 const critical = require('critical');
 const del = require('del');
-
-function extractCssUrls(html, protocol, callback) {
-  var $ = cheerio.load(html);
-  var cssUrls = $('link[rel=stylesheet]')
-    .toArray()
-    .map((linkTag) => sanitizeProtocol(protocol, linkTag.attribs.href));
-
-  callback(null, cssUrls);
-}
+const phantom = require('phantom');
 
 
-function sanitizeProtocol(expectedProtocol, url) {
-  return url.replace(/((https?:)?\/\/)/, `${expectedProtocol}//`);
-}
+function getPageAndCssContent(url, callback) {
+  phantom.create(function (ph) {
+    ph.createPage(function (page) {
+      page.set('settings.webSecurityEnabled', false);
 
-function getCssContent(html, protocol, callback) {
-  extractCssUrls(html, protocol, function (err, cssUrls) {
-    async.reduce(cssUrls, '', function (memo, cssUrl, callback) {
-      request.get(cssUrl, function (error, response, body) {
-        callback(null, memo + body);
+      page.open(url, function (status) {
+        page.evaluate(
+          function () {
+
+            var sheets = [].slice.apply(document.styleSheets);
+            var result;
+
+            result = sheets.reduce(function (memo, sheet) {
+              var rules;
+              if (!sheet.cssRules) {
+                return memo;
+              }
+
+              rules = [].slice.apply(sheet.cssRules);
+              return memo + rules.reduce(function (previousRuleText, rule) {
+                  return previousRuleText + rule.cssText;
+                }, '');
+            }, '');
+
+            return result;
+            return JSON.stringify(result);
+          },
+          function (cssContent) {
+            page.get('content', function (content) {
+              callback(null, {
+                cssContent: cssContent,
+                pageContent: content
+              });
+              ph.exit();
+            });
+          });
       });
-
-    }, function (err, result) {
-      callback(null, result);
     });
   });
 }
@@ -37,24 +49,21 @@ function getCssContent(html, protocol, callback) {
 
 module.exports = {
   generateFromUrl: function (url, callback) {
-    request.get(url, function (error, response, body) {
-      var protocol = URL.parse(url).protocol;
-      getCssContent(body, protocol, function (err, cssContent) {
-        var cssFilePath = mktemp.createFileSync('tmp-XXXXX.css');
-        fs.writeFileSync(cssFilePath, cssContent);
+    getPageAndCssContent(url, function (err, content) {
+      var cssFilePath = mktemp.createFileSync('tmp-XXXXX.css');
+      fs.writeFileSync(cssFilePath, content.cssContent);
 
-        critical.generate({
-          base: '.',
-          html: body,
-          inline: false,
-          css: cssFilePath,
-          width: 1300,
-          height: 900,
-          minify: true,
-        }, function (err, criticalCss) {
-          del.sync('tmp-*');
-          return callback(err, criticalCss);
-        });
+      critical.generate({
+        base: '.',
+        html: content.pageContent,
+        inline: false,
+        css: cssFilePath,
+        width: 1300,
+        height: 900,
+        minify: true,
+      }, function (err, criticalCss) {
+        del.sync('tmp-*');
+        return callback(err, criticalCss);
       });
     });
   }
--- spec/generateCriticalCssIntegrationSpec.js
+++ spec/generateCriticalCssIntegrationSpec.js
@@ -1,69 +1,20 @@
-const proxyquire = require('proxyquire');
+const path = require('path');
 
 describe('Generate critical css', function () {
   it('should generate critical css by provided url', function (done) {
-    const url = 'http://noassetmovement.jimdo.com';
-    var request = require('request');
+    const http = require('http');
+    const app = require('connect')();
+    const criticalCssGenerator = require('../generate-critical-css');
+    const serveStatic = require('serve-static');
+    var server;
+    app.use(serveStatic(path.join(__dirname, 'fixtures')));
+    server = http.createServer(app);
+    server.listen(3004);
 
-    const markup = getMarkupFixture();
-
-    // css returned by first css file in document
-    const css = `
-      #styleme {color: #000}
-      #idonotexist {color: green}
-    `;
-
-    // css returned by second css file in document
-    const anotherCss = `
-      #styleme2 {color: #fff}
-      #idonotexist {color: black}
-    `;
-
-
-    // mock request and return css and markup content, depending on url requested
-    // in order to avoid actual requests being made
-    spyOn(request, 'get').andCallFake(function (url, callback) {
-      if (url.endsWith('style.css')) {
-        return callback(null, {}, css);
-      }
-
-      if (url.endsWith('style2.css')) {
-        return callback(null, {}, anotherCss);
-      }
-
-      return callback(null, {}, markup);
-
-    });
-
-    const criticalCssGenerator = proxyquire('../generate-critical-css', {
-      'request': request
-    });
-
-    criticalCss = criticalCssGenerator.generateFromUrl(url, function(err, criticalCss) {
-      // only the applied css should be included, also should be minified
+    criticalCssGenerator.generateFromUrl('http://localhost:3004', function (err, criticalCss) {
       expect(criticalCss).toEqual('#styleme{color:#000}#styleme2{color:#fff}');
+      server.close();
       done();
     });
-
   });
 });
-
-function getMarkupFixture() {
-  return `
-
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-  <link href="style.css" rel="stylesheet" />
-  <link href="style2.css" rel="stylesheet" />
-
-  <body>
-    <div id="styleme">CONTENT</div>
-    <div id="styleme2">CONTENT</div>
-  </body>
-</head>
-
-</html>
-
-  `;
-}
