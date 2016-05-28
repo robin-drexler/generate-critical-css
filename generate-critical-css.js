a7f281eeb3bfbd68360c19d5d401d64ef9f5bfef
Robin Drexler <drexler.robin@gmail.com>
2016-05-28 21:14:13 +0200

    Upgrade to phantom 2

--- generate-critical-css.js
+++ generate-critical-css.js
@@ -3,53 +3,64 @@ const fs = require('fs');
 const critical = require('critical');
 const del = require('del');
 const phantom = require('phantom');
-
+var pageInstance;
+var phantomInstance;
+var css;
 
 function getPageAndCssContent(url, callback) {
-  phantom.create(function (ph) {
-    ph.createPage(function (page) {
-      page.set('settings.webSecurityEnabled', false);
 
-      page.open(url, function (status) {
-        page.evaluate(
-          function () {
 
-            var sheets = [].slice.apply(document.styleSheets);
-            var result;
+  phantom.create()
+    .then(function(ph) {
+      phantomInstance = ph;
+      return ph.createPage();
+    })
+    .then(function(page) {
+      pageInstance = page;
+      return page.setting('webSecurityEnabled', false);
+    })
+    .then(function() {
+      return pageInstance.open(url);
+    })
+    .then(function(status) {
+      return pageInstance.evaluate(function () {
+        var sheets = [].slice.apply(document.styleSheets);
+        var result;
 
-            result = sheets.reduce(function (memo, sheet) {
-              var rules;
-              if (!sheet.cssRules) {
-                return memo;
-              }
+        result = sheets.reduce(function (memo, sheet) {
+          var rules;
+          if (!sheet.cssRules) {
+            return memo;
+          }
 
-              rules = [].slice.apply(sheet.cssRules);
-              return memo + rules.reduce(function (previousRuleText, rule) {
-                  return previousRuleText + rule.cssText;
-                }, '');
+          rules = [].slice.apply(sheet.cssRules);
+          return memo + rules.reduce(function (previousRuleText, rule) {
+              return previousRuleText + rule.cssText;
             }, '');
+        }, '');
 
-            return result;
-            return JSON.stringify(result);
-          },
-          function (cssContent) {
-            page.get('content', function (content) {
-              callback(null, {
-                cssContent: cssContent,
-                pageContent: content
-              });
-              ph.exit();
-            });
-          });
+        return result;
+        return JSON.stringify(result);
+      })
+    })
+    .then(function(cssContent) {
+      css = cssContent;
+      return pageInstance.property('content');
+    })
+    .then(function(content) {
+      callback(null, {
+        cssContent: css,
+        pageContent: content
       });
+      phantomInstance.exit();
     });
-  });
 }
 
 
 module.exports = {
   generateFromUrl: function (url, callback) {
     getPageAndCssContent(url, function (err, content) {
+
       var cssFilePath = mktemp.createFileSync('tmp-XXXXX.css');
       fs.writeFileSync(cssFilePath, content.cssContent);
 
@@ -61,7 +72,7 @@ module.exports = {
         width: 1300,
         height: 900,
         minify: true,
-      }, function (err, criticalCss) {
+      }, function (err,  criticalCss) {
         del.sync('tmp-*');
         return callback(err, criticalCss);
       });
